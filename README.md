a5798b9e8528907a77dc6bf1085645eeec589cd5
jaswanth098 <jaswanth@deepcompute.com>
2017-08-23 13:38:50 +0200

    refernece #1 uploading intial code with readme.md

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
 # jslogger
 Logging Javascript logs to server.
+## Dependencies
+Jslogger need jquery to be imported.
+## Usage
+Download the src/jslogger-bundle.js and copy to desired location.
+Include jslogger.js in your page using below code.
+```javascript=
+<script type="text/javascript" src="jslogger-bundle.js"></script>
+```
+## Initialize jslogger in your webpage
+Initializing jslogger.js
+```javascript=
+<script type="text/javascript">
+    var log = new jslogger({url:url, max_logs:max_logs, time_ms:time_ms});
+</script>
+```
+- url - Replace this with backend url where a function is listening for post request with a parameter **log**.
+- max_logs - Maximum number of logs to be packed and send to server at a time (optional default 1000).
+- time_ms - At what time interval all the logs needs to be checked and sent to server (optional default 5000ms).
+
+Jslogger can bind user(string), if it is a login based website, by using:
+```javascript=
+log.bind({user:user, key:'additional info about user'})
+```
+The additional information will be logged with the key specified.
+
+Jslogger supports the following console methods for logging in below way:
+```javascript=
+log.message(message, {data:"anykind of data"});
+log.info(message, {data:"anykind of data"});
+log.log(message, {data:"anykind of data"});
+log.error(message, {data:"anykind of data"});
+log.debug(message, {data:"anykind of data"});
+log.warn(message, {data:"anykind of data"});
+```
+#### Example
+```bash
+log.info('testing log.info', {data:(new Date()).getTime()})
+```
+Jslogger can log exceptions using log.exception
+#### Example
+```javascript=
+try {
+    throw new Error('testing jslogger');
+}
+catch(e){
+    log.exception(e.message);
+}
+```
+## Log ajax calls
+- Jslogger can log ajax calls by calling a function as **log.ajax** similar to **$.ajax** but in addition to it, jslogger log the time taken, start time, end time, ajax url, etc.
+```javascript=
+log.ajax({
+    url: url,
+    type: GET/POST,
+    data: data,
+    success: function(response){
+        log.log('any message if needed' + {response: response});
+    }
+});
+```
+Jslogger even logs ajax call failure automatically.
+
+- Can log getJSON calls by calling a function as **log.getJSON** instead of **$.getJSON**
+```javascript=
+ log.getJSON(url, function(response){
+      // your code goes here
+ })
+```
+- Can also log **\$.get** and **\$.post** in the above manner using **log.get** and **log.post**
+
+- When ever there is a javascript error it captures and logs it.
+- It uses localstorage in browser to store all the logs, even if the browser crashes the logs will not be destroyed. They will be logged to server when the page loads again.
+- Jslogger uses Epoch time(Unix time) for logging.
+#### server side
+- The logs will be logged via ajax call in post method with a variable **log**. The server side script should be able to take the request with argument **log**. Below is a example of form data for ajax call.
+```javascript=
+{
+log:[
+        {
+        UUID: '181393b0-8702-11e7-9ca6-a7e5a0572c550.8393907533887879', //sortable unique number
+        level : info/log/debug/error/user message/warn/exception/ajax,
+        message: 'message',
+        user: 'user if specified',
+        url: 'url where log happened',
+        host_url: 'host url(domain)',
+        misc: 'any extra data user specifies that is used while logging'
+        },
+        {}, {}, ....
+    ]
+}
+```
---
+++ src/jslogger.js
@@ -0,0 +1,322 @@
+;(function() {
+    'use strict';
+
+    function jslogger(req) {
+
+        this.ajax      = ajax;
+        this.getJSON   = getJSON;
+        this.get       = get;
+        this.post      = post;
+        this.exception = exception;
+        this.info      = info;
+        this.error     = error;
+        this.debug     = debug;
+        this.log       = log;
+        this.warn      = warn;
+        this.message   = msg;
+        this.bind      = bind;
+
+        var queue           = 0,
+            count           = 0,
+            url             = req.url,
+            timeInterval    = req.time_ms ? req.time_ms : 5000,
+            maxLogs         = req.max_logs ? req.max_logs : 1000,
+            ajaxCall        = ajaxCall,
+            common          = common,
+            appender        = appender,
+            user            = null,
+            user_detail     = null,
+            isAjaxCompleted = true;
+
+        function bind(req){
+
+            user = req.user;
+            delete req.user;
+            user_detail = req;
+        }
+
+        // Get data from all kinds of messages that are being logged and form a structed log.
+        function common(options) {
+
+            var time = new Date();
+            // TODO use window.location.host
+            var source = window.location.host
+            var data = {
+                UUID      : uuidv1(),
+                timestamp : time.getTime(),
+                message   : options.message,
+                level     : options.type,
+                url       : options.url,
+                host_url  : source,
+                misc      : options.misc
+            }
+
+            if(user != null) {
+                data.user = user;
+            }
+            if(user_detail != null) {
+                for(var key in user_detail) {
+                    data[key] = user_detail[key];
+                }
+            }
+
+            appender(data);
+            return;
+        }
+
+        // store data to localStorage
+        function appender(data) {
+
+            count += 1;
+            window.localStorage.setItem("logging_" + data.UUID, JSON.stringify(data));
+            if(maxLogs == 0) {
+                return;
+            }
+
+            if(count > maxLogs) {
+                ajaxCall();
+                count = 0;
+            }
+            return;
+        }
+
+        // Data for logging functions
+        function log_data(level, msg, url, misc){
+            var data = {
+                type    : level,
+                message : msg,
+                url     : url,
+                misc    : misc
+            }
+            common(data);
+            return;
+        }
+
+
+        // Log different kinds of console logging methods
+        function info() {
+
+            log_data("info", arguments[0], window.location.href, arguments[1]);
+            console.info(arguments[0]);
+            return;
+        }
+
+        function error() {
+
+            log_data("exception", arguments[0], window.location.href, arguments[1]);
+            console.error(arguments[0]);
+            return;
+        }
+
+        function debug() {
+
+            log_data("debug", arguments[0], window.location.href, arguments[1]);
+            console.debug(arguments[0]);
+            return;
+        }
+
+        function log() {
+
+            log_data("log", arguments[0], window.location.href, arguments[1]);
+            console.log(arguments[0]);
+            return;
+        }
+
+        function warn() {
+
+            log_data("warn", arguments[0], window.location.href, arguments[1]);
+            console.warn(arguments[0]);
+            return;
+        }
+
+        function msg() {
+
+            log_data("msg", arguments[0], window.location.href, arguments[1]);
+            return;
+        }
+
+        function exception() {
+
+            log_data("exception", arguments[0], window.location.href, arguments[1]);
+            console.error(arguments[0]);
+            return;
+        }
+
+        function xhrStatus(msg, req_url, xhr, startTime, is_call_success) {
+
+            var endTime = new Date().getTime(),
+                data   = {
+                status          : xhr.status,
+                ready_state     : xhr.readyState,
+                status_message  : xhr.statusText,
+                url             : req_url,
+                start_time_ms   : startTime,
+                end_time_ms     : endTime,
+                request_time_ms : endTime-startTime,
+                response_length : xhr.responseText.length
+            }
+
+            if(!is_call_success) {
+                data.response = xhr.responseText.substring(0,2000);
+            }
+
+            common({message:msg, misc:data});
+            return;
+        }
+
+        // Log ajax call
+        function ajax(data) {
+
+            var startTime = new Date().getTime(),
+                xhr       = ($.ajax(data));
+
+            xhr.success(function() {
+                xhrStatus('ajax success', data.url, xhr, startTime, true);
+            });
+
+            xhr.fail(function() {
+                xhrStatus('ajax success', data.url, xhr, startTime, false);
+            });
+
+            return xhr;
+        }
+
+        // Log getJSON call
+        function getJSON(req_url, fun) {
+
+            var startTime = new Date().getTime(),
+                xhr       = $.getJSON(req_url, fun);
+            xhr.success(function() {
+                xhrStatus('getJSON success', req_url, xhr, startTime, true);
+            });
+
+            xhr.fail(function() {
+                xhrStatus('getJSON failed', req_url, xhr, startTime, false);
+            });
+
+            return xhr;
+        }
+
+        // Log get call
+        function get(req_url, fun) {
+
+            var startTime = new Date().getTime(),
+                xhr       = $.get(req_url, fun);
+            xhr.success(function() {
+                xhrStatus('get request success', req_url, xhr, startTime, true);
+            });
+
+            xhr.fail(function() {
+                xhrStatus('get request failed', req_url, xhr, startTime, false);
+            });
+
+            return xhr;
+        }
+
+        // Log post call
+        function post(req_url, fun) {
+
+            var startTime = new Date().getTime(),
+                xhr       = $.post(req_url, fun);
+            xhr.success(function() {
+                xhrStatus('post request success', req_url, xhr, startTime, true);
+            });
+
+            xhr.fail(function() {
+                xhrStatus('post request failed', req_url, xhr, startTime, false);
+            });
+
+            return xhr;
+        }
+
+        // flush logs into server
+        function ajaxCall() {
+
+            if(isAjaxCompleted) {
+                flushPendingLogs();
+            }
+        }
+
+        /**
+         * Send pending logs to url which is provided by checking logs that are being logged.
+         */
+        function flushPendingLogs() {
+
+            isAjaxCompleted  = false;
+            var pending_logs = 0,
+                data         = [],
+                log_keys     = [];
+
+            for(var i in window.localStorage){
+                pending_logs = i.startsWith('logging') ? pending_logs + 1 : pending_logs;
+            }
+
+            if(pending_logs == 0) {
+                isAjaxCompleted = true;
+                return;
+            }
+
+            for(var key_index in window.localStorage) {
+                if(key_index.startsWith('logging') && data.length < 1000) {
+                    data.push(JSON.parse(window.localStorage.getItem(key_index)));
+                    log_keys.push(key_index);
+                }
+            }
+
+            var params = {
+                log : data,
+            }
+
+            var xhr = ajax({
+                url     : url,
+                type    : 'POST',
+                data    : JSON.stringify(params),
+                timeout : 1000 * 60 * 10,
+                success : function() {
+                    for(var key_index in log_keys) {
+                        delete window.localStorage[log_keys[key_index]]
+                    }
+                },
+            });
+
+            xhr.done(function(response) {
+
+                pending_logs -= 100;
+                if(pending_logs > 0) {
+                    flushPendingLogs();
+                }
+
+                isAjaxCompleted = true;
+            });
+        }
+
+        // Time interval for sending logs to url
+        setInterval(function() {
+            ajaxCall();
+        }, timeInterval);
+
+        // capture javascript errors
+        window.onerror = function(message, source, lineno, colno, error) {
+
+            var time = new Date(),
+                data = {
+                "UUID"      : uuidv1(),
+                "timestamp" : time.getTime(),
+                "message"   : message,
+                "url"       : source,
+                "lineno"    : lineno,
+                "colno"     : colno,
+                "error"     : error,
+                "level"     : "javascript",
+                "host_url"  : window.location.host
+            }
+
+            appender(data);
+            return;
+        }
+
+        ajaxCall();
+    }
+
+window.jslogger = jslogger;
+})();
